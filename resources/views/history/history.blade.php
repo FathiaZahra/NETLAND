a91ddb220990bf4769c4223f5a1d213762c71812
kesyaa1123 <kesyaafiana30@gmail.com>
2024-02-19 21:51:18 +0700

    sbchs

--- resources/views/history/history.blade.php
+++ resources/views/history/history.blade.php
@@ -1,46 +1,66 @@
 @extends('layout.layout')
-@section('title','Tambah Barang')
+@section('title', 'History')
 @section('content')
-<div class="row">
-    <div class="col-md-12">
-        <div>
-            <div class="card-header">
-                <span class="h1">
-                    History
-                </span>
-                <hr>
-            </div>
-            <br>
-            <div class="card-body">
-                <form method="POST">
-                    @csrf
-                    <div class="row">
-                        <div class="col-12">
-                                @foreach ($logs as $data)
-                                <div class="card" style="padding:10px;">
-                                    <span>Log : {{ $data->log }}</span>
-                                </div>
-                                <br> 
-                                @endforeach
-                        </div>
-                    </div>
-                </form>
-            </div>
-            <div class="card-footer">
+    <div class="row">
+        <div class="col-md-12">
+            <div>
+                <div class="card-header">
+                    <span class="h1">
+                        History
+                    </span>
+                    <hr>
+                </div>
+                <br>
+                <div class="card-body">
+                    <form method="POST">
+                        @csrf
+                        <div class="row">
+                            <div class="col-12">
+                                @foreach ($transaksi as $data)
+                                    @if (Auth::user()->role == 'pengelola')
+                                        @if (!Str::startsWith($data->log, 'staff_ticketing') && !Str::startsWith($data->log, 'penyewaan'))
+                                            <div class="card" style="padding:10px;">
+                                                <span>Log : {{ $data->log }}</span>
+                                            </div>
+                                        @endif
+                                    @endif
 
+                                    @if (Auth::user()->role == 'staff_ticketing')
+                                        @if (!Str::startsWith($data->log, 'pengelola') && !Str::startsWith($data->log, 'penyewaan'))
+                                            <div class="card" style="padding:10px;">
+                                                <span>Log : {{ $data->log }}</span>
+                                            </div>
+                                            <br>
+                                        @endif
+                                    @endif
+
+                                    @if (Auth::user()->role == 'staff_penyewaan')
+                                        @if (!Str::startsWith($data->log, 'ticketing') && !Str::startsWith($data->log, 'pengelola'))
+                                            <div class="card" style="padding:10px;">
+                                                <span>Log : {{ $data->log }}</span>
+                                            </div>
+                                        @endif
+                                    @endif
+                                @endforeach
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                <div class="card-footer">
+
+                </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
 
 @section('footer')
-<script>
-    document.getElementById('checkAll').addEventListener('click', function() {
-        var checkbox = document.querySelectorAll('.checkbox');
-        for (var i = 0; i < checkbox.length; i++) {
-            checkbox[i].checked = !checkbox[i].checked;
-        }
-    })
-</script>
+    <script>
+        document.getElementById('checkAll').addEventListener('click', function() {
+            var checkbox = document.querySelectorAll('.checkbox');
+            for (var i = 0; i < checkbox.length; i++) {
+                checkbox[i].checked = !checkbox[i].checked;
+            }
+        })
+    </script>
 @endsection
